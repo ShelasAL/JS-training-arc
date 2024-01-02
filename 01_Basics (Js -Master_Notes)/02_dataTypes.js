3abc8d85d20c1dbe876ddbfce1e56c3dba4cd690
Shelas AL <shelasastray@gmail.com>
2024-01-02 18:24:01 +0000

    datatypes

---
+++ 01_Basics (Js -Master_Notes)/02_dataTypes.js
@@ -0,0 +1,34 @@
+//---------------------------------------Guide and Tips------------------------------------------ 
+
+"use strict";  //treat all js code as newer js (whole file will become new strict,as of now it works auto)
+
+// alert("Yaro")  // we are running it in nodejs, not browser so we gets error
+
+console.log(5          //low code redability
+    +5)
+console.log(3+2)       //code readability should be high
+
+//-------------------------------------------Data Types -----------------------------------------
+
+let Username = "Unimon"        //string
+let age = "16"                //Number
+let isLoggedIn = false       //Boolean
+
+//number => 2^53
+//bigint => used to scale this number
+//string => "",''
+//boolean => true/false
+//null => standalone value
+//undefined => yet to assign value 
+//symbol => used for uniqueness
+
+
+// object
+
+
+//-------------------------------------------typeof-----------------------------------------------
+console.log(typeof(age))           //string
+console.log(typeof(true))         //boolean
+console.log(typeof(null))        //object
+console.log(typeof(undefined))  // undefined
+
