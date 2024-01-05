983c593f6ffd9d88d5552e6c03152ca7f22b6c1d
Shelas AL <shelasastray@gmail.com>
2024-01-05 18:06:04 +0000

    dataTypes_part2

--- 01_Basics/06_dataTypes_pt2.js
+++ 01_Basics/06_dataTypes_pt2.js
@@ -16,8 +16,8 @@
              //* Objects   { name : baka}
              //* Functions -> function() {   }
 
-             // Non premitive type returns dataType as Object 
-             //type of function returns "Function" (but its called as Function Object)
+             // Non premitive type returns dataType as Object [typeof(primitiveFunction) = object]
+             // typeof(function) returns "Function" (but its called as Function Object)
 
  //~-------------------------------------Intro to Symbol-----------------------------------------------            
  //* Symbols are unique Id given to a variable
@@ -25,7 +25,11 @@
  //^ ex:-
          const myId = Symbol("1234") 
          const ourId = Symbol("1234")
-         console.log(myId === ourId)
+         console.log(myId === ourId)  //false
+
+         /*Both of the id's are unique and strictly equals to returns false as the symbol of value
+          is stored  as a unique value in the given variable*/
+          
 
 
          
